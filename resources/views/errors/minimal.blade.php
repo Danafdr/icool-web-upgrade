1705c1139910866d54bf27f47d0f4bfd88f0b17e
Danadirsha <danadirshafdr@gmail.com>
2026-06-01 04:24:14 +0700

    fix: resolve infinite loop on error pages by fixing Blade syntax

--- resources/views/errors/minimal.blade.php
+++ resources/views/errors/minimal.blade.php
@@ -58,10 +58,12 @@
         <div class="error-message">@yield('message')</div>
         @hasSection('action_onclick')
             <a href="javascript:void(0)" onclick="@yield('action_onclick')" class="btn">@yield('action_text')</a>
-        @elseif(View::hasSection('action_url'))
-            <a href="@yield('action_url')" class="btn">@yield('action_text')</a>
         @else
-            <a href="/" class="btn">Kembali ke Beranda</a>
+            @hasSection('action_url')
+                <a href="@yield('action_url')" class="btn">@yield('action_text')</a>
+            @else
+                <a href="/" class="btn">Kembali ke Beranda</a>
+            @endif
         @endif
     </div>
 </body>
