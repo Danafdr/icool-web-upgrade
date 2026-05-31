a7e8c37b26d3e9e6a157bf08de35242214f1f591
Danadirsha <danadirshafdr@gmail.com>
2026-06-01 02:38:55 +0700

    fix: syntax error in DashboardOverview dialog footer

--- resources/js/pages/Admin/DashboardOverview.tsx
+++ resources/js/pages/Admin/DashboardOverview.tsx
@@ -583,6 +583,7 @@ export default function DashboardOverview({ stats, forms, serviceTypes = [], fil
                                 )}
                             </Button>
                         )}
+                        </div>
                     </DialogFooter>
                 </DialogContent>
             </Dialog>
