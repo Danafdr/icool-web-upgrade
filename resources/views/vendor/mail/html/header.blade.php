7ea45f0c03c8cd35a08fe389caa21a8eeaa0fc98
Danadirsha <danadirshafdr@gmail.com>
2026-06-01 02:20:55 +0700

    style: replace laravel email logo with icool logo

---
+++ resources/views/vendor/mail/html/header.blade.php
@@ -0,0 +1,8 @@
+@props(['url'])
+<tr>
+<td class="header">
+<a href="{{ $url }}" style="display: inline-block;">
+<img src="https://icool-web-upgrade.vercel.app/images/logo.png" class="logo" alt="iCool Logo" style="height: 50px; width: auto; object-fit: contain;">
+</a>
+</td>
+</tr>
